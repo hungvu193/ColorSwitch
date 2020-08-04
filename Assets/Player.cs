59a419042b7c0371c28ddb2a0196fd67e3b49046
hungvu <visaocuoitroi193@gmail.com>
2020-08-04 09:58:34 +0700

    init color switch

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = jumpForce * Vector2.up;
+        }
+    }
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hello");
+        Debug.Log(collision.tag);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.tag);
+    }
+}
